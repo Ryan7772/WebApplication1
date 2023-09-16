5a9abd0c978ffdd0f035b06b6b8bec3d7f780ec2
RYAN <RYAN@RYAN>
2023-09-15 21:08:09 -0300

    Adicionar arquivos de projeto.

---
+++ ClassLibrary1/Interfaces/Interface.cs
@@ -0,0 +1,31 @@
+﻿using FluentValidation;
+using Modelo.Domain.Entidade;
+
+namespace Modelo.Domain.Interfaces
+
+{
+    public interface IBaseRepository<TEntity> where TEntity : BaseEntity
+    {
+        void Insert(TEntity obj);
+
+        void Update(TEntity obj);
+
+        void Delete(int id);
+
+        IList<TEntity> Select();
+
+        TEntity Select(int id);
+    }
+    public interface IBaseService<TEntity> where TEntity : BaseEntity
+    {
+        TEntity Add<TValidator>(TEntity obj) where TValidator : AbstractValidator<TEntity>;
+
+        void Delete(int id);
+
+        IList<TEntity> Get();
+
+        TEntity GetById(int id);
+
+        TEntity Update<TValidator>(TEntity obj) where TValidator : AbstractValidator<TEntity>;
+    }
+}
