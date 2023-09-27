5afcd71362880b2f577d28fd69a7c17929fd2375
Ryan7772 <ryanwillrich730@gmail.com>
2023-09-27 15:20:47 -0300

    sssss

---
+++ ClassLibrary3/Repositorio/Interfaces/IAlunoRepositorio.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo.infra.Data.Repositorio.Interfaces
+{
+    internal class IAlunoRepositorio
+    {
+    }
+}
